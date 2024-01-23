a3fb43ef5091f2c075861c59511da8bb002c7ac4
Fabrice Uwayezu <uwafabrice01@gmail.com>
2024-01-23 11:20:33 -0800

    Update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,46 @@
-import re
+#!/usr/bin/python3
+
+""" script that reads stdin line by line and computes metrics """
+
 import sys
 
-total_size = 0
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
-}
-lines_processed = 0
+
+def printStatus(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
+
+
+# sourcery skip: use-contextlib-suppress
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
 
 try:
     for line in sys.stdin:
-        lines_processed += 1
-        match = re.search(r"(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(?P<date>.*)\] \"GET /projects/260 HTTP/1.1\" (?P<status_code>\d{3}) (?P<file_size>\d+)", line)
-        if match:
-            try:
-                file_size = int(match.group("file_size"))
-                total_size += file_size
-                status_codes[match.group("status_code")] += 1
-            except ValueError:
-                pass  # Ignore invalid file sizes
+        if count != 0 and count % 10 == 0:
+            printStatus(statusCodes, size)
 
-        if lines_processed % 10 == 0:
-            print("File size:", total_size)
-            for code in sorted(status_codes):
-                count = status_codes[code]
-                if count > 0:
-                    print(f"{code}: {count}")
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except Exception:
+            pass
+
+        try:
+            if stlist[-2] in statusCodes:
+                statusCodes[stlist[-2]] += 1
+        except Exception:
+            pass
+    printStatus(statusCodes, size)
+
 
 except KeyboardInterrupt:
-    print("File size:", total_size)
-    for code in sorted(status_codes):
-        count = status_codes[code]
-        if count > 0:
-            print(f"{code}: {count}")
+    printStatus(statusCodes, size)
+    raise
